6209de50ab4a1f3dc9bddc5f3e5ce1c5beb32359
Alifkm <alifkahfi@gmail.com>
2023-06-05 09:35:42 +0700

    feat: Adding emojis

---
+++ src/components/Emojis.tsx
@@ -0,0 +1,21 @@
+import meh from "../assets/Emojis/meh.webp";
+import bullsEye from "../assets/Emojis/bulls-eye.webp";
+import thumbsUp from "../assets/Emojis/thumbs-up.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+const Emojis = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojisMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+  return <Image {...emojisMap[rating]} marginTop={2} />;
+};
+
+export default Emojis;
